ad4f1d1bbe393a8ab5c69e09e6ee6d962ee9a509
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-07 15:01:51 +0300

    User can sign in

--- Gemfile
+++ Gemfile
@@ -29,4 +29,3 @@ group :development do
 end
 
 
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "todos#index"
 
   resources :todos, only: [:index, :new, :create]
+  resource :session, only: [:new, :create]
 end
