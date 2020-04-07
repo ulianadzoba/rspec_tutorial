969cb3590b64556f0d6baf786fe36727c161ec11
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-07 22:06:17 +0300

    Allow todo to be completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_162240) do
+ActiveRecord::Schema.define(version: 2020_04_07_184310) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.datetime "completed_at"
   end
 
 end
