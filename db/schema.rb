a6af53fc9480b67d235ea245a1657b7f74463001
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-07 20:29:51 +0300

    Show todo only for user with appropriate email

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_094522) do
+ActiveRecord::Schema.define(version: 2020_04_07_162240) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -8,6 +8,6 @@ feature "User creates todo" do
     fill_in "Title", with: "Buy milk"
     click_on "Submit"
     
-    expect(page).to have_css '.todos li', text: 'Buy milk'
+    expect(page).to have_css ".todos li", text: "Buy milk"
   end
 end
